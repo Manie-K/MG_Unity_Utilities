5c07ae2a53dff91a3b1bf24d0e9d5da643feb22e
Maciej Góralczyk <83976515+Manie-K@users.noreply.github.com>
2024-11-09 19:15:47 +0100

    Update TransformExtensions.cs

--- Runtime/Scripts/ExtensionMethods/TransformExtensions.cs
+++ Runtime/Scripts/ExtensionMethods/TransformExtensions.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace MG_Utilities
 {
@@ -6,8 +7,28 @@ namespace MG_Utilities
     {
         public static void DeleteChildren(this Transform t)
         {
-            foreach(Transform child in t) 
-                Object.Destroy(child.gameObject);
+            if (Application.isEditor)
+            {
+                foreach (Transform child in t)
+                    Object.DestroyImmediate(child.gameObject);
+            }
+            else
+            {
+                foreach (Transform child in t)
+                    Object.Destroy(child.gameObject);
+            }
+        }
+
+        public static List<T> GetComponentsInDirectChildren<T>(this Transform t) where T : Component
+        {
+            List<T> components = new List<T>();
+            foreach (Transform child in t)
+            {
+                T component = child.GetComponent<T>();
+                if (component != null)
+                    components.Add(component);
+            }
+            return components;
         }
     }
 }
